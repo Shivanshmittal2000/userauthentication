5ccbed059e6fc051d7bb5ab8235ebe320e849f57
Shivansh <shivanshmittal2000@gmail.com>
2021-06-26 23:45:21 +0530

    In this i hae modified files related to edit and delte operations

--- userauthenticate/login/urls.py
+++ userauthenticate/login/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.login,name='login'),
     path('signup/',views.signup,name='signup'),
-    path('show/<int:id>/',views.show,name='show')
+    path('show/<int:id>/',views.show,name='show'),
+    path('show/<int:id>',views.deletedata,name='deletedata'),
+    path('showuser/<int:id>',views.useredit,name='userediting'),
+    path('showemail/<int:id>',views.emailedit,name='emailediting'),
+    path('showaddress/<int:id>',views.addressedit,name='addressediting')
 ]
--- userauthenticate/login/views.py
+++ userauthenticate/login/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import User
 import json
@@ -11,13 +12,15 @@ def login(request):
         requireduser=User.objects.filter(email=email)
         print(requireduser[0].password,password)
         if requireduser[0].password == password:
-            newlist=[]
-            # print(users)
-            for i in users:
-                newlist.append(i)
-            print(newlist)
-            print(newlist[0])
-            return render(request,'login/show.html',{'userdetail':requireduser[0], 'users':newlist})
+            return show(request,requireduser[0].id)
+
+            # newlist=[]
+            # # print(users)
+            # for i in users:
+            #     newlist.append(i)
+            # print(newlist)
+            # print(newlist[0])
+            # return render(request,'login/show.html',{'userdetail':requireduser[0], 'users':users})
         # print(type(requireduser))
         # print(requireduser[0].id)
         # print(requireduser)
@@ -59,9 +62,122 @@ def show(request,id):
     print(id)
     if request.method=='POST':
         users=User.objects.all()
-        newlist=[]
         print(users)
-        for i in users:
-            newlist.append(i)
-    # print(newlist,hello)
-    return render(request,'login/show.html',{'users':newlist})
+        idfilter=User.objects.filter(id=id)
+        print(idfilter[0])
+        username=request.POST.get('username','')
+        userfilter=User.objects.filter(username=username)
+        print(len(userfilter))
+        email=request.POST['email']
+        emailfilter=User.objects.filter(email=email)
+        print(len(emailfilter))
+    return render(request,'login/show.html',{'users':users,'userdetail':idfilter[0],'ischange':True})
+
+def deletedata(request,id):
+    if request.method=="POST":
+        users=User.objects.all()
+        filterid=User.objects.get(id=id)
+        print(filterid)
+        filterid.address= ""
+        print(filterid.address)
+        filterid.save()
+        print(filterid.address)
+        # print(filterid[0])
+        return HttpResponseRedirect('/')
+
+def useredit(request,id):
+    print('user',id)
+    if request.method=="POST":
+        users=User.objects.all()
+        print(users)
+        iffil=User.objects.filter(id=id)
+        idfilter=User.objects.get(id=id)
+        print(idfilter)
+        username=request.POST.get('updatedinfo','')
+        print(username)
+        userfilter=User.objects.filter(username=username)
+        print(len(userfilter))
+        alreadyusername=idfilter.username
+        if len(userfilter)>0:
+            ischange=True
+            if alreadyusername==username:
+                ischange="username is same as previous one "
+                # print("username is same as previous one ")
+                return render(request,'login/show.html',{'users':users,'userdetail':iffil[0],'ischange':ischange})
+    
+            else :
+                print("this name is taken by some other user please chose another username")
+                ischange="this name is taken by some other user please chose another username"
+                return render(request,'login/show.html',{'users':users,'userdetail':iffil[0],'ischange':ischange})
+        else :
+            idfilter.username=username
+            idfilter.save()
+            print(idfilter.username)
+            return HttpResponseRedirect('/')
+
+def emailedit(request,id):
+    print('email',id)
+    if request.method=="POST":
+        users=User.objects.all()
+        print(users)
+        iffil=User.objects.filter(id=id)
+        idfilter=User.objects.get(id=id)
+        print(idfilter)
+        email=request.POST.get('updatedinfo','')
+        password=request.POST.get('updatepassword','')
+        passwordfilter=User.objects.filter(password=password)
+        emailfilter=User.objects.filter(email=email)
+        alreadypassword=idfilter.password
+        alreadyemail=idfilter.email
+        if len(emailfilter)>0:
+            if alreadyemail==email:
+                # print("email is same as previous one ")
+                ischange="email is same as previous one"
+                if alreadypassword==password:
+                    ischange +="This password is already taken by you"
+                elif password == "":
+                    ischange += "password cannot be blank"
+                else :
+                    idfilter.email=email
+                    idfilter.password=password
+                    idfilter.save()
+                    print(idfilter.email)
+                    print(idfilter.password)
+                    return HttpResponseRedirect('/')
+                
+            else :
+                ischange="Email already registered choose another email"
+            return render(request,'login/show.html',{'users':users,'userdetail':iffil[0],'ischange':ischange})
+        else :
+            if alreadypassword==password:
+                ischange +="This password is already taken by you"
+            elif password == "":
+                ischange += "password cannot be blank"
+            else :
+                idfilter.email=email
+                idfilter.password=password
+                idfilter.save()
+                print(idfilter.email)
+                print(idfilter.password)
+                return HttpResponseRedirect('/')
+
+        
+
+
+
+
+
+def addressedit(request,id):
+    if request.method == "POST":
+        print('address',id)
+    if request.method=="POST":
+        users=User.objects.all()
+        print(users)
+        idfilter=User.objects.get(id=id)
+        print(idfilter)
+        address=request.POST.get('updatedinfo','')
+        idfilter.address=address
+        idfilter.save()
+        print(idfilter.address)
+        return HttpResponseRedirect('/')
+    
